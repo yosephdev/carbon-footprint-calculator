2d20b1f1f3569336192ff283cf8ab5dbb8dd99c0
Yoseph Berhane <yosephbet@gmail.com>
2023-10-04 09:49:40 +0200

    Add footer information

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+// Footer.js
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>© 2023 Carbon Footprint Calculator. All Rights Reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
