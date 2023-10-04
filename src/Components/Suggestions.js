97b96297ce568a851ba5d1373330eae183f577c5
Yoseph Berhane <yosephbet@gmail.com>
2023-10-04 09:50:40 +0200

    Add display suggestions to reduce carbon footprint

---
+++ src/Components/Suggestions.js
@@ -0,0 +1,20 @@
+// Suggestions.js
+import React from "react";
+
+const Suggestions = () => {
+  return (
+    <div>
+      <h2>Suggestions to Reduce Your Carbon Footprint</h2>
+      <ul>
+        <li>
+          Use public transportation or carpooling instead of driving alone.
+        </li>
+        <li>Switch to energy-efficient appliances and light bulbs.</li>
+        <li>Plant trees and maintain a garden.</li>
+        {/* Add more suggestions */}
+      </ul>
+    </div>
+  );
+};
+
+export default Suggestions;
