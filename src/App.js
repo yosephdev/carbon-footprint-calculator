dd623994810900fd74f3f053958d4d5ee0cbb216
Yoseph Berhane <yosephbet@gmail.com>
2023-10-04 10:01:59 +0200

    Set up routing  and render the main components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Calculator from "./components/Calculator";
+import Suggestions from "./components/Suggestions";
+import Tracker from "./components/Tracker";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/calculator" element={<Calculator />} />
+            <Route path="/suggestions" element={<Suggestions />} />
+            <Route path="/tracker" element={<Tracker />} />
+            <Route
+              path="/"
+              element={<h1>Welcome to Carbon Footprint Calculator</h1>}
+            />
+            {/* You can add more content or components for the landing page */}
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
